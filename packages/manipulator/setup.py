6e31e2ad12f129840d90d556ac1b1de262a2c1d7
Jihyun Jeon <hedgehog412@gmail.com>
2024-02-25 15:56:05 -0800

    init ROS

---
+++ packages/manipulator/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'manipulator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+        ('share/' + package_name + '/meshes', glob('meshes/*')),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
+        ('share/' + package_name + '/launch', glob('launch/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'kinematics = manipulator.kinematics:main'
+        ],
+    },
+)
