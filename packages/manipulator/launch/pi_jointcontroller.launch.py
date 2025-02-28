df4d6d378f8ab71439ba2d3ada2929afe0feb520
Jyun Jeon <hedgehog412@gmail.com>
2025-02-27 23:51:16 -0800

    adding pi launch file

---
+++ packages/manipulator/launch/pi_jointcontroller.launch.py
@@ -0,0 +1,114 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory as pkgdir
+
+from launch                      import LaunchDescription
+from launch.actions              import Shutdown, RegisterEventHandler
+from launch_ros.actions          import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
+from launch_ros.substitutions import FindPackageShare
+
+#
+# Generate the Launch Description
+#
+def generate_launch_description():
+
+    ######################################################################
+    # LOCATE FILES
+
+    # Locate the RVIZ configuration file.
+    rvizcfg = os.path.join(pkgdir('manipulator'), 'rviz/viewurdf.rviz')
+
+    # Locate/load the robot's URDF file (XML).
+    urdf = os.path.join(pkgdir('manipulator'), 'urdf/joints.urdf')
+    with open(urdf, 'r') as file:
+        robot_description = file.read()
+
+    robot_controllers = os.path.join(pkgdir('manipulator'), 'config/joint_controller.yaml')
+
+
+    ######################################################################
+    # PREPARE THE LAUNCH ELEMENTS
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+    )
+
+    # Configure a node for the point_publisher.
+    node_robot_state_publisher_COMMAND = Node(
+        name       = 'robot_state_publisher', 
+        package    = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        output     = 'screen',
+        parameters = [{'robot_description': robot_description}],
+        remappings = [('/joint_states', '/joint_commands')])
+    
+    node_demo = Node(
+        name       = 'JointControl',
+        package    = 'manipulator',
+        executable = 'JointControl',
+        output     = 'screen',
+        on_exit    = Shutdown())
+
+    # Configure a node for RVIZ
+    node_rviz = Node(
+        name       = 'rviz', 
+        package    = 'rviz2',
+        executable = 'rviz2',
+        output     = 'screen',
+        arguments  = ['-d', rvizcfg],
+        on_exit    = Shutdown())
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--param-file", robot_controllers],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["velocity_controller", "--param-file", robot_controllers],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[node_rviz],
+        )
+    )
+
+    # Delay start of joint_state_broadcaster after `robot_controller`
+    # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
+    delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_controller_spawner,
+            on_exit=[joint_state_broadcaster_spawner],
+        )
+    )
+
+
+    ######################################################################
+    # RETURN THE ELEMENTS IN ONE LIST
+
+    return LaunchDescription([
+
+        # Start the demo and RVIZ
+        node_robot_state_publisher_COMMAND, 
+        control_node,
+        robot_controller_spawner,
+        # joint_state_broadcaster_spawner,
+        delay_joint_state_broadcaster_after_robot_controller_spawner,
+        # delay_rviz_after_joint_state_broadcaster_spawner,
+        node_demo,
+        # node_rviz,
+    ])
