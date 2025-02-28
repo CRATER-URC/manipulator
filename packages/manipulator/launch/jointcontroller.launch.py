65d9693a2fb7ff0e1189dd196461f445d2b5d26b
Jyun Jeon <hedgehog412@gmail.com>
2025-02-27 21:54:40 -0800

    changed launch config

--- packages/manipulator/launch/jointcontroller.launch.py
+++ packages/manipulator/launch/jointcontroller.launch.py
@@ -76,7 +76,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["forward_position_controller", "--param-file", robot_controllers],
+        arguments=["forward_command_controller", "--param-file", robot_controllers],
     )
 
     # Delay rviz start after `joint_state_broadcaster`
